e3a4d67d079780130820178a45b1a3b56f942b3c
theHenriqueFerreira <henrique@alphanetprovedor.com.br>
2024-10-13 14:29:00 -0300

    Update limpa_memoria.sh
    ajuste script

--- limpa_memoria.sh
+++ limpa_memoria.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#
 echo "Limpando a memória cache..."
 
 # Sincroniza os dados do sistema de arquivoscom o disco
@@ -11,14 +11,14 @@ echo "Cache de página limpa."
 sleep 2
 
 # Limpa a página de dentries e inodes
-echo 2 > /proc/sys/vm/drop_caches
-echo "Dentries e inodes limpos."
-sleep 2
+#echo 2 > /proc/sys/vm/drop_caches
+#echo "Dentries e inodes limpos."
+#sleep 2
 
 # Limpa todaa memória cache (páginas, dentries e inodes)
-echo 3 > /proc/sys/vm/drop_caches
-echo "Toda a memória cache limpa."
-sleep 2
+#echo 3 > /proc/sys/vm/drop_caches
+#echo "Toda a memória cache limpa."
+#sleep 2
 
 # Informa que a limpeza foi concluída
 echo "Memória cache limpa com sucesso!"
