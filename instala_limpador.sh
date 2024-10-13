3bda1905c750aae2495812e02ae48a3e4af431ba
theHenriqueFerreira <henrique@alphanetprovedor.com.br>
2024-10-13 12:43:18 -0300

    Update instala_limpador.sh
    criação do setup de instalação

--- instala_limpador.sh
+++ instala_limpador.sh
@@ -1,1 +1,17 @@
 
+#!/bin/bash
+
+SCRIPT_NAME="limpa_memoria.sh"
+SCRIPT_URL="https://raw.githubusercontent.com/theHenriqueFerreira/limpa_memoria/main/$SCRIPT_NAME" 
+
+wget $SCRIPT_URL -O $SCRIPT_NAME
+chmod +x $SCRIPT_NAME
+
+HORA=$(whiptail --inputbox "Escolha a hora para executar o script diariamente (0-23):" 10 60 0 --title "Configuração do Cron" 3>&1 1>&2 2>&3)
+
+CRON_JOB="$HORA 0 * * * /usr/local/bin/$SCRIPT_NAME"
+(crontab -l; echo "$CRON_JOB") | crontab -
+
+sudo mv $SCRIPT_NAME /usr/local/bin/
+
+echo "Instalação concluída! O script será executado diariamente às $HORA:00."
