f13fc0582f21a88d592fbb387c2f149852670bac
theHenriqueFerreira <henrique@alphanetprovedor.com.br>
2024-10-13 12:40:25 -0300

    Update limpa_memoria.sh
    script de limpeza

--- limpa_memoria.sh
+++ limpa_memoria.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+
+echo "Limpando a memória cache..."
+sync; echo 1 > /proc/sys/vm/drop_caches
+sync; echo 2 > /proc/sys/vm/drop_caches
+sync; echo 3 > /proc/sys/vm/drop_caches
+echo "Memória cache limpa com sucesso!"
 
